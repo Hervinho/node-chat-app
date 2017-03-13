4dd81887d7be28a655de7637c6cd4e8ca5440f28
Hervinho <herventubi@gmail.com>
2017-03-13 14:29:33 +0200

    generateMessage and test cases

--- server/server.js
+++ server/server.js
@@ -4,6 +4,8 @@ const express = require('express');
 const publicPath = path.join(__dirname, '../public');
 const socketIO = require('socket.io');
 
+var {generateMessage} = require('./utils/message');
+
 const PORT = process.env.PORT || 2000;
 
 var app = express();
@@ -17,36 +19,18 @@ io.on('connection', (socket) => {
   console.log('New user connected');
 
   //This is emitted to a newly connected user.
-  socket.emit('newMessage', {
-    from: 'ADMIN',
-    text: 'Welcome to chat app',
-    joinedAt: new Date().getTime()
-  });
+  socket.emit('newMessage', generateMessage('ADMIN', 'Welcome to chat app'));
 
   //emit this event to all connnections in chat room EXCEPT the new user.
-  socket.broadcast.emit('newMessage', {
-    from: 'ADMIN',
-    text: 'New User joined chat room',
-    joinedAt: new Date().getTime()
-  });
+  socket.broadcast.emit('newMessage', generateMessage('ADMIN', 'New User joined chat'));
 
   //Listen to custom event from client to server.
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
 
     //emit this event to ALL connnections
-    io.emit('newMessage', {
-      from: message.from,
-      text: message.text,
-      createdAt: new Date().getTime()
-    });
+    io.emit('newMessage', generateMessage(message.from, message.text));
 
-    //emit this event to all connnections EXCEPT the selected socket.
-    /*socket.broadcast.emit('newMessage', {
-      from: message.from,
-      text: message.text,
-      createdAt: new Date().getTime()
-    });*/
   });
 
   socket.on('disconnect', () => {
---
+++ server/utils/message.test.js
@@ -0,0 +1,13 @@
+const expect = require('expect');
+var {generateMessage} = require('./message');
+
+describe('generateMessage', () => {
+  it('should generate correct message object', () => {
+    var from = 'Me';
+    var text = 'Yo!';
+    var message = generateMessage(from, text);
+
+    expect(message.createdAt).toBeA('number');
+    expect(message).toInclude({from, text});
+  });
+});
