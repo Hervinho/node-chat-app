11ae1dca320d357fdba70724d449fda4e2797395
Hervinho <herventubi@gmail.com>
2017-03-14 15:35:43 +0200

    Add getUser and removeUser methods + test cases

--- server/utils/users.js
+++ server/utils/users.js
@@ -7,6 +7,29 @@ class Users{
     this.users.push(user);
     return user;
   }
+  removeUser(id){
+    //first, find user to be removed
+    var userToRemove = this.getUser(id);
+
+    //if user was found
+    if(userToRemove){
+      this.users = this.users.filter((user) => user.id !== id);
+    }
+
+    return userToRemove;
+  }
+  getUser(id){
+    return this.users.filter((user) => user.id === id)[0];
+  }
+  getUserList(room){
+    //get users of a chat room.
+    var users = this.users.filter((user) => user.room === room);
+
+    //get users' names by converting array of objects into array of strings
+    var userNamesArray = users.map((user) => user.name);
+
+    return userNamesArray;
+  }
 }
 
 module.exports = {Users};
--- server/utils/users.test.js
+++ server/utils/users.test.js
@@ -2,15 +2,84 @@ const expect = require('expect');
 const {Users} = require('./users');
 
 describe('Users', () => {
+  var usersArray;
+
+  beforeEach(() => {
+    usersArray = new Users();
+    usersArray.users = [{
+      id: '1',
+      name: 'Ironman',
+      room: 'Marvel'
+    },{
+      id: '2',
+      name: 'Batman',
+      room: 'DC Comics'
+    }, {
+      id: '3',
+      name: 'Superman',
+      room: 'DC Comics'
+    }];
+  });
+
   it('should add new User', () => {
-    var userArray = new Users();
+    var usersArray = new Users();
     var user = {
       id: '12',
       name: 'Hervinho',
       room: 'General'
     };
 
-    var resultUser = userArray.addUser(user.id, user.name, user.room);
-    expect(userArray.users).toEqual([user]);
+    var resultUser = usersArray.addUser(user.id, user.name, user.room);
+    expect(usersArray.users).toEqual([user]);
+  });
+
+  it('should return names for DC Comics', () => {
+    var roomName = 'DC Comics';
+    var usersList = usersArray.getUserList(roomName);
+
+    expect(usersList).toEqual(['Batman', 'Superman']);
+  });
+
+  it('should return names for Marvel', () => {
+    var roomName = 'Marvel';
+    var usersList = usersArray.getUserList(roomName);
+
+    expect(usersList).toEqual(['Ironman']);
+  });
+
+  it('should find a user', () => {
+    var id = '1';
+    var user = usersArray.getUser(id);
+
+    expect(user).toEqual(usersArray.users[0]);
+  });
+
+  it('should not find a user', () => {
+    var id = '10';
+    var user = usersArray.getUser(id);
+
+    expect(user).toNotExist();
+  });
+
+  it('should remove a user', () => {
+    var id = '1';
+    var user = usersArray.removeUser(id);
+
+    //ensure such user to be found.
+    expect(user.id).toEqual(id);
+
+    //ensure user was removed
+    expect(usersArray.users).toNotInclude(user);
+  });
+
+  it('should not remove a user', () => {
+    var id = '11';
+    var user = usersArray.removeUser(id);
+
+    //ensure such user not to be found.
+    expect(user).toNotExist();
+
+    //ensure usersArray has not changed.
+    expect(usersArray.users.length).toEqual(3);
   });
 });
