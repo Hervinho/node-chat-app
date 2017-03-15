83bdcba118bed0da4bf096138fe385522d300a94
Hervinho <herventubi@gmail.com>
2017-03-15 10:49:11 +0200

    Code cleanup

--- server/utils/users.js
+++ server/utils/users.js
@@ -33,24 +33,3 @@ class Users{
 }
 
 module.exports = {Users};
-
-/*
-//ES6 class syntax
-class Person{
-  //constructor
-  constructor (name, age){
-    this.age = age;
-    this.name = name;
-  }
-
-  //methods
-  getUserDescription (){
-    return `${this.name} is ${this.age} year(s) old.`;
-  }
-}
-
-var me = new Person('Hervinho', 26);
-var desc = me.getUserDescription();
-console.log(me.name, me.age);
-console.log(desc);
-*/
